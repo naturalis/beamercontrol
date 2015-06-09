925529aee2c29e844963dbfcb482693c56aba468
Atze de Vries <atze.devries@naturalis.nl>
2015-06-09 14:39:47 +0200

    cleanup

--- api/run.rb
+++ api/run.rb
@@ -4,9 +4,6 @@ require 'net/telnet'
 require 'resolv'
 Dir["#{File.join(File.expand_path(File.dirname(__FILE__)).split('/')[0..-1])}/../lib/*.rb"].each { |l| require l}
 
-#require './lib/telnet.rb'
-
-
 get '/' do
   erb :index
 end
@@ -14,28 +11,11 @@ end
 get '/types/' do
   content_type :json
   list.to_json
-  # list = Array.new
-  # Dir["./beamers.d/*.yaml"].each do |f|
-  #   b = YAML.load_file(f)
-  #   list << b['name']
-  # end
-  # {:types => list}.to_json
 end
 
 get '/types/:type' do
   content_type :json
-  #puts params[:type]
   beameroptions(params[:type]).to_json
-  #options(params[:type]).to_json
-  # options = Array.new
-  # begin
-  #   YAML.load_file("./beamers.d/#{params[:type]}.yaml")['commands'].each do |k,v|
-  #     options << k
-  #   end
-  #   { :options => options }.to_json
-  # rescue
-  #   "Could not load beamertype: #{params[:type]}".to_json
-  # end
 end
 
 post '/command/:cmd' do
